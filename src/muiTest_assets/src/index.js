03131e2bf8f84e0e56a99d12ca63bf15b235a472
aginamena5 <aginamena5@gmail.com>
2023-01-07 11:20:54 -0500

    Initial commit.

---
+++ src/muiTest_assets/src/index.js
@@ -0,0 +1,19 @@
+import { muiTest } from "../../declarations/muiTest";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await muiTest.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
